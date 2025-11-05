80d899b5ba412490a32337ea608288f288cf9023
Viss666 <vistral9546@gmail.com>
2025-11-05 13:35:48 -0700

    added kleitor

--- bot.py
+++ bot.py
@@ -283,6 +283,12 @@ give_items = {
             "type":"file",
             "content":["gifs/mub1.gif","gifs/mub2.gif","gifs/mub3.gif","gifs/mub4.gif","gifs/mub5.gif","gifs/mub6.gif","gifs/mub7.gif","gifs/mub8.gif","gifs/mub9.gif","gifs/mub10.gif","gifs/mub11.gif","gifs/mub12.gif","gifs/mub13.gif","gifs/mub14.gif","gifs/mub15.gif","gifs/mub16.gif","gifs/mub17.gif","gifs/mub18.gif","gifs/mub19.gif","gifs/mub20.gif","gifs/mub21.gif","gifs/mub22.gif","gifs/mub23.gif","gifs/mub24.gif","gifs/mub25.gif"]
         }
+    ],
+    "kleitor": [
+        {
+            "type":"file",
+            "content":["gifs/kleitor1.gif"]
+        }
     ]
 }
 
